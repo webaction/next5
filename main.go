941910b1236d8dd3521fbc2e3b1605d5fbd5fb4b
Mischa King <mischa@webaction.com.au>
2017-11-15 09:53:26 +1000

    Added HTTP Serve

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
+	"flag"
+)
+
+var (
+	addr      = flag.String("addr", "127.0.0.1:9999", "http service address")
 )
 
 func main() {
 	fmt.Println("Starting Server")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/races", GetRaces).Methods("GET")
+
+	httpErr := http.ListenAndServe(*addr, router)
+	if httpErr != nil {
+		log.Fatal("ListenAndServe: ", httpErr)
+	}
 }
