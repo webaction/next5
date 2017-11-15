d638db142fc8d9ce25d930354ec7b4c60c0930ac
Mischa King <mischa@webaction.com.au>
2017-11-15 10:14:47 +1000

    Added data

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import (
 	"log"
 	"flag"
 	"encoding/json"
+	"github.com/icrowley/fake"
+	"time"
+	"math/rand"
 )
 
 var (
-	addr  = flag.String("addr", "127.0.0.1:9999", "http service address")
-	races []Race
+	addr      = flag.String("addr", "127.0.0.1:9999", "http service address")
+	races     []Race
+	raceTypes []string
 )
 
 type Race struct {
@@ -40,6 +44,53 @@ type Competitors struct {
 	Name     string `json:"name"`
 }
 
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func randomTimestamp(i int) int64 {
+
+	randomTime := time.Now().Unix() + (int64(i) * 600)
+
+	randomNow := time.Unix(randomTime, 0).Unix()
+
+	return randomNow
+}
+
+func buildDataSet() []Race {
+	raceTypes = []string{"Thoroughbred", "Greyhounds", "Harness"}
+
+	var race []Race
+	for i := 0; i < 1000; i++ {
+		idx := random(0, 2)
+		suspend := randomTimestamp(i)
+		name := fake.FullName()
+
+		var competitors []Competitors
+		for h := 1; h < random(4, 23); h++ {
+			competitors = append(competitors, Competitors{Position: h, Name: fake.FullName()})
+		}
+
+		race = append(race, Race{Description: name,
+			Suspend: suspend,
+			EventID: random(10000, 50000),
+			Details: Details{
+				RaceType: raceTypes[idx],
+				RaceNum:  i,
+				Country:  "AUS",
+				Meeting: Meeting{
+					ID:   random(100, 9999),
+					Name: name,
+					Date: time.Unix(suspend, 0).String(),
+				},
+			},
+			Competitors: competitors,
+		})
+	}
+
+	return race
+}
+
 func GetRaces(writer http.ResponseWriter, request *http.Request) {
 
 	writer.Header().Set("Access-Control-Allow-Origin", "*")
@@ -49,6 +100,8 @@ func GetRaces(writer http.ResponseWriter, request *http.Request) {
 
 func main() {
 	fmt.Println("Starting Server")
+
+	races = buildDataSet()
 
 	router := mux.NewRouter()
 	router.HandleFunc("/api/races", GetRaces).Methods("GET")
